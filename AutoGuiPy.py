23043dca9c57713faddbbfbcabe1d55960558c0e
JamesGuillermo <j12cachero@gmail.com>
2025-07-18 06:02:21 +0800

    finding the venv and the interpreter location

--- AutoGuiPy.py
+++ AutoGuiPy.py
@@ -1,5 +1,6 @@
 import pyautogui
 import time
+import sys
 
 # Wait for you to switch to Excel
 time.sleep(3)
@@ -15,10 +16,19 @@ pyautogui.write("Bob, 30, UK", interval=0.1)'''
 print("Press Ctrl + C in the terminal to stop.\n")
 
 # Print screen resolution once
-print("Screen size:", pyautogui.size())
+#print("Screen size:", pyautogui.size())
 
-while True:
+'''while True:
     # Get current mouse position
     x, y = pyautogui.position()
     print(f"Mouse position: X={x}, Y={y}", end="\r")  # overwrite line
     time.sleep(0.1)  # update every 0.1 second
+
+'''
+print(sys.path)
+print(pyautogui.__file__)
+
+import sys
+sys.path.insert(0, r"C:\Users\James Cachero\OneDrive\Documents\PythonProgramming\Data_Entry_Python_Program\.venv\Lib\site-packages")
+
+print(sys.executable)
